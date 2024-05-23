b27f83d55e90f644277f4db027d4de50d87f9939
tizianosanse <157717522+tizianosanse@users.noreply.github.com>
2024-05-23 17:19:09 +0200

    finto esercizio 2

--- u4-d4-w1/src/Main.java
+++ u4-d4-w1/src/Main.java
@@ -1,14 +1,27 @@
-import esercizio.Dipartimento;
-import esercizio.Dipendenti;
-import esercizio.DipendentiPartTime;
+import esercizio.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         DipendentiPartTime JENNY = new DipendentiPartTime(132434,1000,Dipartimento.PRODUZIONE,80);
+        DipendenteFullTime BRUNO = new DipendenteFullTime(222564,1500,Dipartimento.VENDITE);
+        Dirigente JERRY = new Dirigente(342434,2000,Dipartimento.AMMINISTRAZIONE);
+        DipendentiPartTime TOM = new DipendentiPartTime(672436,1000,Dipartimento.PRODUZIONE,85);
+        DipendenteFullTime JHON = new DipendenteFullTime(232537,1500,Dipartimento.VENDITE);
         JENNY.calculateSalary();
-        System.out.println();
+        int stipendi = 0;
+        Dipendenti[] arrDipen = {JHON,JENNY,JERRY,BRUNO,TOM};
+        for (int i = 0; i < arrDipen.length ; i++) {
+
+             stipendi = stipendi + arrDipen[i].getStipendio();
+            System.out.println(stipendi);
+
+
+
+        }
+
+
 //        Dipendenti JACOPO = new Dipendenti(123213,2000, Dipartimento.AMMINISTRAZIONE);
 //        Dipendenti GIOVA = new Dipendenti(753753,1600, Dipartimento.VENDITE);
 //        Dipendenti TOMMY = new Dipendenti(56780,1300, Dipartimento.PRODUZIONE);
